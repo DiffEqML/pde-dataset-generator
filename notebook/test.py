d7c49ed128f1065fadcf275085b3e6bf4f4cc848
cbhua <cbhua@kaist.ac.kr>
2021-05-21 13:57:09 +0900

    Reconstruct Repo

--- notebook/test.py
+++
@@ -1,9 +0,0 @@
-from fenics import *
-
-import matplotlib.pyplot as plt
-import numpy as np
-from mshr import *
-
-domain = Rectangle(Point(-2, -2), Point(2, 2))
-mesh = generate_mesh(domain, 16)
-function_space = FunctionSpace(mesh, 'P', 1)
