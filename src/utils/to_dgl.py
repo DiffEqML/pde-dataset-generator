f8d7fb1d3934608b0a755d4b8fc9a1c339de651a
Federico Berto <berto.federico2@gmail.com>
2021-06-23 23:46:46 +0900

    Update to_dgl method for cross-compatibility

--- src/utils/to_dgl.py
+++ src/utils/to_dgl.py
@@ -1,140 +1,67 @@
 import dgl
 import torch
 import numpy as np
-from fenics import *
-from dolfin import *
-from mshr import *
+
+'''
+The SIMULATOR_to_graph methods return the following values:
+    - nodes: int, number of nodes of the graph
+    - edges: list, format [source, destination]
+    - keys: list of strings
+    - values: list of tensors, corresponding to the keysfrom .arcsim import arcsim_to_graph
+'''
+from .arcsim import arcsim_to_graph
+from .fenics import fenics_to_graph
+from .su2 import su2_to_graph
+
+def to_dgl(data=None,
+           mesh=None,
+           function=None,
+           method=None,
+           **kwargs):
+    '''Obtain DGL graphs from different methods
+    Supported file types:
+        - arcsim (.obj)
+        - su2 (.su2, .vtu, .vtk)
+    If the method is not provided, we check automatically
+    Example kwargs: 
+        - keys: list of specific field keys to look for
+        - gen_mode: use graph element networks
+    '''
+    # Check for method override
+    if method is not None:
+        if method == 'arcsim':
+            graph_data = arcsim_to_graph(data, **kwargs)
+        elif method == 'fenics':
+            graph_data = fenics_to_graph(mesh, function, **kwargs)
+        elif method == 'su2':
+            graph_data = su2_to_graph(data, mesh, **kwargs)
+        else:
+            raise ValueError("Method not supported. Currently available methods are arcsim, fenics and su2.")
+    elif data is not None:
+        # Automatic file detection
+        import os
+        filename, file_extension = os.path.splitext(data)
+        if file_extension == '.obj':
+            graph_data = arcsim_to_graph(data, **kwargs)
+        elif file_extension == '.vtu' or file_extension == '.vtk':
+            graph_data = su2_to_graph(data, mesh, **kwargs)
+        else:
+            raise ValueError('Detected file type is {}. Currently readable types for graph data are .obj, .bin, .vtu and .vtk').format(file_extension)
+    elif mesh is not None and function is not None:
+        graph_data = fenics_to_graph(mesh, function, **kwargs)
+    else: 
+        raise ValueError('No data provided!')
+    # Graph is in a common for all the methods
+    nodes, edges, keys, values = graph_data
+    return create_dgl_graph(nodes, edges, keys, values)
 
 
-def to_dgl(u, mesh):
-    '''
-    TODO: select premeter you want
-    '''
+def create_dgl_graph(nodes, edges, keys, values):
+    '''Minimalistic approach to graph creation in DGL'''
     graph = dgl.DGLGraph()
-    src, dst = get_edges(mesh)
-    graph.add_nodes(mesh.num_vertices())
-    graph.add_edges(src, dst)
-    graph.ndata['x'] = torch.tensor(mesh.coordinates()[:,0])
-    graph.ndata['y'] = torch.tensor(mesh.coordinates()[:,1])
-    graph.ndata['value'] = torch.tensor(get_values(u, mesh))
+    graph.add_nodes(nodes)
+    graph.add_edges(edges[0], edges[1]) # edges: [sources, destinations]
+    for k, i in zip(keys, range(len(keys))):
+        graph.ndata[k] = values[i]
     return graph
 
-
-def get_edges(mesh):
-    ''' Get edges for graph from mesh
-
-    Get two connectivity lists of nodes, edges are directed from
-    src to dst, required by DGL in the graph creation.
-    
-    Example:
-        Edge 0->0, 0->1, 1->2 will be returned by [0, 0, 1], [0, 1, 2]
-
-    Args:
-        mesh: <dolfin.cpp.mesh.Mesh> dolfin mesh
-        
-    Returns:
-        src: <list> [edge number] source node indexes for edges
-        dst: <list> [edge number] distinate node indexes for edges
-    '''
-    mesh.init(0,1)
-    src = []
-    dst = []
-    for v in vertices(mesh):
-        idx = v.index()
-        neighbors = [Edge(mesh, i).entities(0) for i in v.entities(1)]
-        neighbors = np.array(neighbors).flatten()
-        neighbors = neighbors[np.where(neighbors != idx)[0]]
-        for n in neighbors:
-            src.append(int(idx))
-            dst.append(int(n))
-    return src, dst
-
-
-def get_values(u, mesh):
-    ''' Get values for each mesh node
-
-    Provid a function based on coordinate for mesh nodes, this can 
-    calculate values in each node by index order. Here the function 
-    is mostly get by FEniCS solver. 
-
-    Args:
-        u: <dolfin.function> <any other function> function based on 
-            nodes' coordinates 
-        mesh: <dolfin.cpp.mesh.Mesh> dolfin mesh
-
-    Returns:
-        values: <list> [node number] calculated result by index order
-    '''
-    values = []
-    for pos in mesh.coordinates():
-        values.append(u(Point(pos)))
-    return values
-
-
-def to_dgl_gen(u, mesh):
-    ''' Special usage for GEN model '''
-    graph = dgl.DGLGraph()
-    src, dst = get_edges(mesh)
-    graph.add_nodes(mesh.num_vertices())
-    graph.add_edges(src, dst)
-    graph = add_features(u, mesh, src, dst, graph)
-    return graph
-
-
-def add_features(u, mesh, src, dst, graph):
-    coords = mesh.coordinates()
-    value = np.array(get_values(u, mesh)).reshape(-1, 1)
-    feature = np.hstack([coords, value])
-    zero_array = np.zeros((mesh.num_vertices()))
-    one_array = np.ones((mesh.num_vertices()))
-    onehot_array = np.hstack([zero_array.reshape(-1, 1), one_array.reshape(-1, 1)])
-
-    dist = []
-    for i in range(len(src)):
-        dist.append(np.linalg.norm(coords[dst[i]] - coords[src[i]]))
-    dist = np.array(dist)
-
-    graph.ndata['x'] = torch.tensor(coords[:,0], dtype=torch.float32)
-    graph.ndata['y'] = torch.tensor(coords[:,1], dtype=torch.float32)
-    graph.ndata['coords'] = torch.tensor(coords, dtype=torch.float32)
-    # WARNNING: value shape here is different from original one
-    graph.ndata['value'] = torch.tensor(value, dtype=torch.float32)
-    graph.ndata['feat'] = torch.tensor(feature, dtype=torch.float32)
-    graph.ndata['init_feat'] = torch.tensor(feature, dtype=torch.float32)
-    graph.ndata['is_bdd'] = torch.tensor(zero_array)
-    graph.ndata['type'] = torch.tensor(one_array)
-    graph.ndata['type_onehot'] = torch.tensor(onehot_array)
-
-    graph.edata['dist'] = torch.tensor(dist.reshape(-1, 1), dtype=torch.float32)
-    graph.edata['feat'] = torch.tensor(dist.reshape(-1, 1), dtype=torch.float32)
-    graph.edata['init_feat'] = torch.tensor(dist.reshape(-1, 1), dtype=torch.float32)
-    
-    return graph
-
-def obj_to_dgl(obj):
-    '''Transforms pywavefront object into DGL graph
-    Compatible with ArcSim .obj files
-    ==> TODO <=== 
-    1) fix code for supporting multiple features
-    2) check functions
-    3) add timestamps
-    4) merge functions into a single to_dgl with checks
-    '''
-    vert = np.array(obj.vertices)
-    mesh = np.array(obj.mesh_list[0].faces)
-    # We add the sources and destination through contiguous nodes
-    # We should also repeat by switching indexes to have bidirectional graph
-    # BEWARE: this will have lots of duplicates, don't think DGL can deal with them natively
-    # extend method similar to append
-    src = []
-    dst = []
-    for i, j in zip([0,1,2], [1,2,0]): 
-        src.extend(mesh[:,i].tolist()); dst.extend(mesh[:,j].tolist())
-        src.extend(mesh[:,j].tolist()); dst.extend(mesh[:,i].tolist())
-    graph = dgl.DGLGraph()
-    graph.add_nodes(vert.shape[0])
-    graph.add_edges(src, dst)
-    graph.ndata['x'] = torch.tensor(vert[:,0])
-    graph.ndata['y'] = torch.tensor(vert[:,1])
-    graph.ndata['z'] = torch.tensor(vert[:,2])
-    return graph
